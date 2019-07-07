432001b5469189ee51a9e396ff06fad4c0740ff9
Max Woolf <max@minimaxir.com>
2019-07-06 22:05:19 -0700

    Section count validation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from starlette.applications import Starlette
 from starlette.responses import UJSONResponse
+from collections import Counter
 import gpt_2_simple as gpt2
 import tensorflow as tf
 import uvicorn
@@ -31,7 +32,7 @@ async def encode_mana(card_mana):
     hybrid_costs = ''.join(re.findall(hybrid_pattern, card_mana))
 
     # extract colorless number
-    colorless_pattern = r'\d+'
+    colorless_pattern = r'(\d+)'
     colorless_number = re.findall(colorless_pattern, card_mana)
     if len(colorless_number) > 0:
         colorless_costs = ''.join(['^' * int(colorless_number[0])])
@@ -40,7 +41,7 @@ async def encode_mana(card_mana):
 
     # get normal mana symbols
     normal_costs = re.sub(r'(\{.*?\})', '', card_mana)
-    normal_costs = re.sub(r'\d+', '', normal_costs)
+    normal_costs = re.sub(r'(\d+)', '', normal_costs)
     normal_costs = ''.join(x + x for x in normal_costs)
 
     return '{' + colorless_costs + normal_costs + hybrid_costs + '}'
@@ -77,30 +78,36 @@ async def homepage(request):
         text += '3' + card_mana + "|"
 
     length = MIN_LENGTH
+    good_text = False
+    while not good_text:
+        while '\n' not in text and length <= MAX_LENGTH:
+            text = gpt2.generate(sess,
+                                length=STEP_LENGTH,
+                                temperature=0.7,
+                                top_k=40,
+                                prefix=text,
+                                include_prefix=True,
+                                return_as_list=True
+                                )[0]
+            length += STEP_LENGTH
 
-    while '\n' not in text and length <= MAX_LENGTH:
-        text = gpt2.generate(sess,
-                             length=STEP_LENGTH,
-                             temperature=0.7,
-                             top_k=40,
-                             prefix=text,
-                             include_prefix=True,
-                             return_as_list=True
-                             )[0]
-        length += STEP_LENGTH
+            generate_count += 1
+            if generate_count == 8:
+                # Reload model to prevent Graph/Session from going OOM
+                tf.reset_default_graph()
+                sess.close()
+                sess = gpt2.start_tf_sess(threads=1)
+                gpt2.load_gpt2(sess)
+                generate_count = 0
 
-        generate_count += 1
-        if generate_count == 8:
-            # Reload model to prevent Graph/Session from going OOM
-            tf.reset_default_graph()
-            sess.close()
-            sess = gpt2.start_tf_sess(threads=1)
-            gpt2.load_gpt2(sess)
-            generate_count = 0
+        pattern = r'(.*)(?:\n)'
+        trunc_text = re.search(pattern, text)
 
-    pattern = '(.*)(?:\n)'
-
-    trunc_text = re.search(pattern, text)
+        # ensure there is only one of each section in the generated card
+        counts = Counter(trunc_text)
+        section_ids = ['0', '1', '3', '4', '5', '6', '7', '8', '9']
+        if all([counts[x] == 1 for x in section_ids]):
+            good_text = True
 
     return UJSONResponse({'text': trunc_text.group(1)},
                          headers=response_header)
